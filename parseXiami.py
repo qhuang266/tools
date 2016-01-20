acc7817962b7e39c80ace69dce5878a82779d950
qhuang266 <qhuang266@gmail.com>
2016-01-20 22:44:21 +0800

    Create parseXiami.py

---
+++ parseXiami.py
@@ -0,0 +1,11 @@
+# use to parse xiami source file and get song lists
+
+import re
+
+pattern = re.compile(r'<href="http://www.xiami.com/song/[^>]+>([^<]+)</a>')
+
+with open("f.html") as fp:
+	for line in fp:
+		match = pattern.match(line)
+		if match:
+			print match.group()
